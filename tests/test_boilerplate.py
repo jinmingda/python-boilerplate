5d7ce4b308b9cf123dec632499dbbf5c5d2c63ac
Mingda Jin <jinmingda77@gmail.com>
2020-07-08 23:48:23 -0700

    Parametrized pytest

--- tests/test_boilerplate.py
+++ tests/test_boilerplate.py
@@ -1,6 +1,11 @@
 """Tests for module boilerplate.py."""
+import pytest
+
 from boilerplate.boilerplate import add
 
 
-def test_add_with_integers():
-    assert add(1, 2) == 3
+@pytest.mark.parametrize(
+    "x,y,expected", [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (1, -1, 0)]
+)
+def test_add_with_integers(x, y, expected):
+    assert add(x, y) == expected
