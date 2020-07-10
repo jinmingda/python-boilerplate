78c5150703c6f45de5d6f06d46878d96a9df592a
Mingda Jin <jinmingda77@gmail.com>
2020-07-09 21:34:04 -0700

    Test cached pre-commit in actions

--- tests/test_boilerplate.py
+++ tests/test_boilerplate.py
@@ -7,7 +7,7 @@ from boilerplate.boilerplate import add
 
 
 @pytest.mark.parametrize(
-    "x,y,expected", [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (1, -1, 0)]
+    "x,y,expected", [(1, 2, 3), (-1, -2, -3), (0, 0, 0), (2, -1, 1)]
 )
 def test_add_with_integers(x, y, expected):
     assert add(x, y) == expected
