e900277fbace6ee9d85df75dc0025006b4a530bd
Mingda Jin <jinmingda77@gmail.com>
2020-07-09 17:58:53 -0700

    Add hypothesis to dev deps

--- tests/test_boilerplate.py
+++ tests/test_boilerplate.py
@@ -1,5 +1,7 @@
 """Tests for module boilerplate.py."""
 import pytest
+from hypothesis import given
+from hypothesis import strategies as st
 
 from boilerplate.boilerplate import add
 
@@ -9,3 +11,8 @@ from boilerplate.boilerplate import add
 )
 def test_add_with_integers(x, y, expected):
     assert add(x, y) == expected
+
+
+@given(x=st.integers(), y=st.integers())
+def test_add_is_commutative(x, y):
+    assert add(x, y) == add(y, x)
